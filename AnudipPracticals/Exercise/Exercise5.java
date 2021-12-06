eee6fd71f11e70b4ab153d95edb83629f58d55e1
mohitepooja <46784449+mohitepooja@users.noreply.github.com>
2021-12-06 11:42:48 +0530

    Update Exercise5.java

--- AnudipPracticals/Exercise/Exercise5.java
+++ AnudipPracticals/Exercise/Exercise5.java
@@ -1,1 +1,11 @@
-
+import java.util.Scanner;
+public class Exercise5{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+System.out.println("Input a number: " );
+int n=sc.nextInt();
+for(int i=1;i<=10;i++){
+System.out.println(n+" x "+i+" = "+(n*i));
+}
+}
+}
