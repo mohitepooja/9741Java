5721061b3e407858df68ab78d2f50f036282faef
mohitepooja <46784449+mohitepooja@users.noreply.github.com>
2021-12-06 11:16:02 +0530

    Update Exercise2.java

--- AnudipPracticals/Exercise/Exercise2.java
+++ AnudipPracticals/Exercise/Exercise2.java
@@ -1,1 +1,10 @@
-
+import java.util.Scanner;
+public class Exercise2{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter Number= ");
+int n=sc.nextInt();
+int cube= n*n*n;
+System.out.println("Number is : " +n+ " and cube of "+n+" is : "+cube);
+}
+}
