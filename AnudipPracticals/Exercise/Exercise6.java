fb331c08f9659238167df3b32fb9cf0e92697ca2
mohitepooja <46784449+mohitepooja@users.noreply.github.com>
2021-12-06 11:43:40 +0530

    Update Exercise6.java

--- AnudipPracticals/Exercise/Exercise6.java
+++ AnudipPracticals/Exercise/Exercise6.java
@@ -1,1 +1,14 @@
-
+import java.util.Scanner;
+public class Exercise6{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+System.out.println("Input number: ");
+int a=sc.nextInt();
+if(a>0){
+System.out.println("Number is positive");}
+else if(a<0){
+System.out.println("Number is negative");}
+else{
+System.out.println("Number is not positive and negative");}
+}
+}
