48e1c6033b982386ee6c27c6d7e4a5fb3a83b6a5
mohitepooja <46784449+mohitepooja@users.noreply.github.com>
2021-12-09 15:46:29 +0530

    Create SwitchEx.java

---
+++ AnudipPracticals/Day1/SwitchEx.java
@@ -0,0 +1,12 @@
+public class SwitchEx{
+public static void main(String args[]){
+int choice=300;
+//byte choice=100; or short choice=100;
+switch(choice){
+case 100:System.out.println("One hundred");
+break;
+case 20:System.out.println("Twenty");
+break;
+case 30:System.out.println("Thirty");
+break;
+}}}
