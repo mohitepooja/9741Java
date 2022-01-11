81a2b2d1ff12f0d837323007c77792d7059669c1
mohitepooja <46784449+mohitepooja@users.noreply.github.com>
2022-01-11 12:50:55 +0530

    Create ThrowDemo.java

---
+++ AnudipPracticals/TrtCatchExamples/ThrowDemo.java
@@ -0,0 +1,13 @@
+public class ThrowDemo{
+static void validate(int age){
+if(age<24)
+throw new ArithmeticException("not valid");
+else
+System.out.println("Welcome to the club");
+}
+public static void main(String args[])
+{
+validate(17);
+System.out.println("code remainder");
+
+}}
