d149c822f144b9b78bd73e4106f67f7a9da408c6
mohitepooja <46784449+mohitepooja@users.noreply.github.com>
2022-01-31 21:39:22 +0530

    Add files via upload

---
+++ AnudipPracticals/Jpa_Sprint1/jpasampleupdate/src/main/java/jpasampleupdate/jpasampleupdate/FindStudent.java
@@ -0,0 +1,29 @@
+package jpasampleupdate.jpasampleupdate;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import jpasampleupdate.jpasampleupdate.entity.StudentEntity;
+
+public class FindStudent {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf=Persistence.createEntityManagerFactory("Student_details");  
+        EntityManager em=emf.createEntityManager();  
+          
+        em.getTransaction().begin(); 
+          
+        StudentEntity s=em.find(StudentEntity.class,102);  
+          
+        System.out.println("Student id = "+s.getS_id());  
+        System.out.println("Student Name = "+s.getS_name());  
+        System.out.println("Student Age = "+s.getS_age());  
+          
+        em.getTransaction().commit();  
+        
+        emf.close();  
+        em.close();
+	}
+
+}
