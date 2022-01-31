61d21fefe331b178b5ce97b077b4bb4c2629db46
mohitepooja <46784449+mohitepooja@users.noreply.github.com>
2022-01-31 21:41:25 +0530

    Add files via upload

---
+++ AnudipPracticals/Jpa_Sprint1/sampleinsertex/src/test/java/sampleinsertex/sampleinsertex/AppTest.java
@@ -0,0 +1,20 @@
+package sampleinsertex.sampleinsertex;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
