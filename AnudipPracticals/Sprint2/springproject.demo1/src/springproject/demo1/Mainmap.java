ff06d5f2704f7714d49da28e7577d43891dad7b3
mohitepooja <46784449+mohitepooja@users.noreply.github.com>
2022-02-16 14:19:27 +0530

    Add files via upload

---
+++ AnudipPracticals/Sprint2/springproject.demo1/src/springproject/demo1/Mainmap.java
@@ -0,0 +1,20 @@
+package springproject.demo1;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Mainmap {
+	public static void main(String args[]) {
+	 ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+     HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+     objA.setMessage("I'm object A");
+     objA.getMessage();
+
+     HelloWorld objB = (HelloWorld) context.getBean("helloWorld");
+     objB.getMessage();
+
+	}
+}
+
+
+
